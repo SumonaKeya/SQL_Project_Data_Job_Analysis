5a70f43d20cfac281f73f127989fb644bddfacb6
hp <b1779295@live.tees.ac.uk>
2026-01-29 15:37:34 +0000

    vscode to gitignore

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,2 +1,7 @@
-SELECT
-job_id
+SELECT job_id, job_title, job_location,
+job_schedule_type, salary_year_avg,job_posted_date
+FROM job_postings_fact
+where job_title_short = 'Data Analyst' AND job_location = 'Anywhere' AND
+salary_year_avg is not null
+ORDER BY salary_year_avg DESC
+LIMIT 10;
